300e237010bd2f51b7727ba4b213495f1f322f31
Rianjaidi-code <rianjaidi@gmail.com>
2024-04-26 16:11:16 +0700

    company dashboard& api

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,6 +21,18 @@ class DatabaseSeeder extends Seeder
             'name' => 'Rian Admin',
             'email' => 'rianjaidi@fic16.com',
             'password' => Hash::make('12345678'),
+        ]);
+
+        //data dummy for company
+        Company::create([
+            'name' => 'PT Artek Sinergi Multimedia',
+            'email' => 'artek@arteksinergi.co.id',
+            'address' => '18 OFFICE PARK, LT. 25, SUITE A 2, JL. TB SIMATUPANG KAV. 18, JAKARTA SELATAN.',
+            'latitude' => '-6.266667',
+            'longitude' => '106.833333',
+            'radius_km' => '0.5',
+            'time_in' => '08:00',
+            'time_out' => '17:00',
         ]);
     }
 }
